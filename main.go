c5f9d96f0e8f1c6842fba6bea2fd694cc566130e
Jamie Scollay <jamie@scollay.uk>
2024-03-19 21:44:26 +0000

    Ensure main() is using psqlstore.New() instead of mockstore.New()

--- main.go
+++ main.go
@@ -4,26 +4,16 @@ import (
 	"log"
 
 	"github.com/deltabrot/dependency-injection/handler"
-	"github.com/deltabrot/dependency-injection/model"
-	"github.com/deltabrot/dependency-injection/store/mockstore"
+	"github.com/deltabrot/dependency-injection/store/psqlstore"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Llongfile)
 
-	// var s store.Store
-	// s, err := psqlstore.New()
-	// if err != nil {
-	//   panic(err)
-	// }
-
-	s := mockstore.New()
-
-	s.SetResponse(model.Song{
-		ID:     "1",
-		Title:  "Hello",
-		Artist: "Adele",
-	})
+	s, err := psqlstore.New()
+	if err != nil {
+		panic(err)
+	}
 
 	h := handler.New(s)
 	log.Fatal(h.Run())
